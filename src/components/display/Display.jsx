599e91b5f6f45974469e755f81e5a673a7209c39
Jonathan Quesada <jonnythan.quesada@gmail.com>
2021-02-15 09:08:34 -0800

    removed react-json-view to deploy, crashing app

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ReactJSON from 'react-json-view';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import ReactJSON from 
 
-const Display = ({ display }) => (
-  <ReactJSON
-    src={display}
-  />
-);
+// const Display = ({ display }) => (
+//   <ReactJSON
+//     src={display}
+//   />
+// );
 
-Display.propTypes = {
-  display: PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.array
-  ]).isRequired
-};
+// Display.propTypes = {
+//   display: PropTypes.oneOfType([
+//     PropTypes.object,
+//     PropTypes.array
+//   ]).isRequired
+// };
 
-export default Display;
+// export default Display;
